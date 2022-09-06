1e3f27cae29b94d5eb5d8761ef93e0f59d105b7b
mihailz <mihailzafirovski@gmail.com>
2022-09-06 12:42:48 +0200

    Added ng-zorro

--- src/app/service/table-data-mock.service.ts
+++ src/app/service/table-data-mock.service.ts
@@ -5,5 +5,43 @@ import { Injectable } from '@angular/core';
 })
 export class TableDataMockService {
 
+  private tableData = [
+    {
+      name: "Nick",
+      surname: "Simmons",
+      age: 20,
+      email: "nick@gmail.com"
+    },
+    {
+      name: "Maria",
+      surname: "Sokolova",
+      age: 24,
+      email: "maria@gmail.com"
+    },
+    {
+      name: "Tobby",
+      surname: "Soffia",
+      age: 32,
+      email: "tobby@gmail.com"
+    },
+    {
+      name: "Adam",
+      surname: "Smith",
+      age: 44,
+      email: "adam@gmail.com"
+    },
+    {
+      name: "Monica",
+      surname: "Simmons",
+      age: 19,
+      email: "monica@gmail.com"
+    }
+  ];
+
   constructor() { }
+
+  getTableData() {
+    return this.tableData;
+  }
+
 }
